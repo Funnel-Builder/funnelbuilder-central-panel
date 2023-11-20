458d7845f1d525bc2eb3eb54112b73a2e8698120
mosabberul <mosabberulislam55@gmail.com>
2023-11-20 12:31:07 +0600

    tailwind add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,18 +4,28 @@ export default defineNuxtConfig({
   modules: [
     'nuxt-primevue',
     '@pinia/nuxt',
+    '@nuxtjs/tailwindcss'
   ],
+  primevue: {
+    usePrimeVue: true,
+    options: {
+      unstyled: false,
+      ripple: true
+    },
+    cssLayerOrder: 'tailwind-base,primevue, tailwind-utilities',
+    theme: 'md-light-indigo',
+    components: {
+      include: '*',
+      exclude: ['Galleria', 'Carousel']
+    },
+    directives: {
+      include: ['Ripple', 'Tooltip']
+    }
+  },
   css: [
+    "@/assets/css/tailwind.css",
     "@/assets/scss/main.scss",
     "primevue/resources/themes/md-light-indigo/theme.css",
     "primeicons/primeicons.css",
   ],
-  primevue: {
-    options: {
-      ripple: true
-    },
-    usePrimeVue: true,
-    theme: 'md-light-indigo',
-  }
-
 })
