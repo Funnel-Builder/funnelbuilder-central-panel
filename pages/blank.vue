b498ef34fb383b20717b5ec51e16abfe355f8257
mosabberul55 <mosabberulislam55@gmail.com>
2023-11-22 10:25:58 +0600

    page

---
+++ pages/blank.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mt-8 card flex justify-content-center">
+    <h1 class="text-center">Blank Page</h1>
+  </div>
+</template>
+
+<script setup>
+const date = ref();
+</script>
+<style scoped lang="scss">
+.card {
+  max-width: 500px;
+  margin: 50px auto 0;
+}
+</style>
