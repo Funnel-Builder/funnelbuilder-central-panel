219af600f85a11daa1bc14842bc6b5dd8f3dbf6e
parthobepary <pallobbiswas326@gmail.com>
2023-11-26 15:48:08 +0600

    done without api

--- components/buttons/PrimaryButton.vue
+++ components/buttons/PrimaryButton.vue
@@ -1,11 +1,15 @@
 <template>
-    <div><Button to="link" style="background-color: #5A78AD; border: none; border-radius: 15px;" class="w-full h-10 md:h-12 text-xs md:text-lg focus:shadow-none" :label="text" /></div>
+    <div><Button to="link" style="background-color: #5A78AD; border: none; border-radius: 15px;"
+            class="w-full h-10 md:h-12 text-xs md:text-lg focus:shadow-none">
+            <span class="font-bold w-full text-white flex justify-center items-center rounded-2xl h-10 md:h-12 text-xs md:text-lg">{{ text }}</span>
+        </Button>
+    </div>
 </template>
 <script setup>
 const pros = defineProps({
-    text:{
+    text: {
         type: String,
-        default : ''
+        default: ''
     }
 })
 </script>
