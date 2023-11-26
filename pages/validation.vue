d52d773ac13e555067612c7a5d2474c507c751e6
mosabberul55 <mosabberulislam55@gmail.com>
2023-11-26 22:29:28 +0600

    validation

--- pages/validation.vue
+++ pages/validation.vue
@@ -1,36 +1,33 @@
 
 <template>
   <div class="container card flex justify-content-center">
-    <form @submit="onSubmit" class="flex flex-column gap-2">
-      <div>I've read and accept the terms & conditions.</div>
-      <InputSwitch v-model="value" :class="{ 'p-invalid': errorMessage }" aria-describedby="text-error" />
-      <small id="text-error" class="p-error">{{ errorMessage || '&nbsp;' }}</small>
-      <Button type="submit" label="Submit" />
+    <form @submit="onSubmit" class="">
+      <InputText v-model="title.value.value" :class="{ 'invalid': title.errorMessage.value }" aria-describedby="text-error" />
+      <form-input-error :message="title.errorMessage.value" />
+      <div class="my-4">
+        <Button type="submit" label="Submit" />
+      </div>
     </form>
     <Toast />
   </div>
 </template>
 
 <script setup>
-import { useToast } from 'primevue/usetoast';
 import { useField, useForm } from 'vee-validate';
 
-const { handleSubmit, resetForm } = useForm();
-const { value, errorMessage } = useField('value', validateField);
-const toast = useToast();
-
-function validateField(value) {
-  if (!value) {
-    return 'Value is required.';
+//validation rules
+const {handleSubmit, isSubmitting, handleReset, setErrors} = useForm({
+  validationSchema: {
+    title(value) {
+      if (value) return true
+      return 'Title is required'
+    }
   }
-
-  return true;
-}
+})
+//form fields
+const title = useField('title');
 
 const onSubmit = handleSubmit((values) => {
-  if (values.value) {
-    toast.add({ severity: 'info', summary: 'Form Submitted', detail: 'The form is successfully submitted.', life: 3000 });
-    resetForm();
-  }
+  console.log(values);
 });
 </script>
