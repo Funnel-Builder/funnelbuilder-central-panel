50fb31d91fc802b566515ad7d1b07346439afb7b
Void-Brain70 <dasssanik124102@gmail.com>
2023-11-27 08:34:29 +0600

    asdasd

--- components/home/Banner.vue
+++ components/home/Banner.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bannerBg">
-    <div class="container mx-auto py-12">
+    <div class="container mx-auto py-20">
       <!--     Section One -->
-      <div class="flex flex-col items-center gap-y-8">
+      <div class="flex flex-col items-center gap-y-8 relative pb-20">
         <div class="flex items-center gap-x-2 border rounded-full px-6 py-2">
           <img src="/banner/workIcon.svg" alt="icon">
           <p class="text-[#ffffff] text-[16px] font-[400]">Your Perfect Workspace</p>
@@ -23,6 +23,9 @@
             </div>
           </div>
         </div>
+      </div>
+      <div class="flex justify-center">
+        <img src="/banner/siteBuilder.svg" alt="Banner Image"/>
       </div>
     </div>
   </div>
