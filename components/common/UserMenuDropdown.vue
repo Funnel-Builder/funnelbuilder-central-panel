ee25b96367720ed02cd3af45d25d4980541886bc
mosabberul55 <mosabberulislam55@gmail.com>
2023-11-29 23:40:29 +0600

    fix store to refs

--- components/common/UserMenuDropdown.vue
+++ components/common/UserMenuDropdown.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-if="storeToRefs(authStore.user)" class="flex items-center gap-x-2">
+    <div v-if="authStore.user" class="flex items-center gap-x-2">
       <div class="flex justify-center items-center px-4 py-2 gap-x-2 rounded-full cursor-pointer"
            style=" background-color:#eff1f7;">
         <img src="/landing/userIcon.svg" alt="logo"/>
