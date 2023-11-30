ec100fdeb6d6588044c01378c4591e2548970e33
mosabberul55 <mosabberulislam55@gmail.com>
2023-11-30 11:31:03 +0600

    middleware fix

--- middleware/Unverified.js
+++ middleware/Unverified.js
@@ -1,5 +1,3 @@
-import {navigateTo} from "#app";
-
 export default defineNuxtRouteMiddleware((to, from) => {
     const authStore = useAuthStore()
     if (authStore.isLoggedIn && authStore.isVerified) {
