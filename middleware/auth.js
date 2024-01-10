29f327d1e2a3b25ff96d1513712bf4890dc8d880
Yeasir Arafat <yeasirarafat321@gmail.com>
2024-01-10 10:12:33 +0600

    debugging

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,10 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    const authStore = useAuthStore()
-    if (!authStore.isLoggedIn) {
+    const token = useCookie('token')
+    // const authStore = useAuthStore()
+    // if (!authStore.isLoggedIn) {
+    //     return navigateTo('/login?next=' + to.path)
+    // }
+    if (!token.value) {
         return navigateTo('/login?next=' + to.path)
     }
 })
