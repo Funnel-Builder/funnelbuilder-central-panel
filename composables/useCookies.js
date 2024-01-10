3ac269592ef777a583cbfe5ea33bf20c5ee7bf51
Yeasir Arafat <yeasirarafat321@gmail.com>
2024-01-10 11:30:30 +0600

    debugging

--- composables/useCookies.js
+++ composables/useCookies.js
@@ -1,8 +1,8 @@
 import moment from "moment";
 
 const commonCookieOptions = {
-    priority: 'high',
-    secure: false
+    // priority: 'high',
+    // secure: false
 };
 
 const useTokenCookie = () => useCookie('token', commonCookieOptions);
