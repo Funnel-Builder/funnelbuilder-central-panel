8b9fb0d394714f924aff62c584f5deff1e9140d5
Yeasir Arafat <yeasirarafat321@gmail.com>
2024-01-10 10:18:50 +0600

    debugging

--- pages/test.vue
+++ pages/test.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
 const name = useCookie('test')
-name.value = 'Setting Value'
 
 const clientSideName = ref('')
-onMounted(()=>{
+onMounted(() => {
   clientSideName.value = useCookie('test').value as string
+  name.value = 'Setting Value'
 })
 </script>
 
 <template>
-<div class="mt-10 flex justify-center">
-  Server Side {{name}}
-  <br/>
-  Client Side: {{clientSideName}}
-</div>
+  <div class="mt-10 flex justify-center">
+    Server Side {{ name }}
+    <br/>
+    Client Side: {{ clientSideName }}
+  </div>
 </template>
 
 <style scoped lang="scss">
