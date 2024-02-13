5f17a5f135f4fbed38ff4d0216166e256139ccb4
Anik Chandra <dasssanik124102@gmail.com>
2024-02-13 11:58:05 +0600

    contact us

--- components/buttons/ActionButton.vue
+++ components/buttons/ActionButton.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <nuxt-link v-if="type == 'link'" :to="to" :class="`${classes} flex justify-center items-center rounded-md h-10 md:h-12 text-xs md:text-lg`">{{ text }}</nuxt-link>
-        <button v-else :disabled="disabled" @click="$emit('submitData')" :class="`${disabled ? disabledClass : activeClass} ${classes} flex items-center justify-center`">{{ text }} <i v-if="loading" class="pi pi-spin pi-spinner" style="font-size: 1rem; margin-left: 5px;"></i></button>
+        <button v-else :disabled="disabled" type="submit" @click="$emit('submitData')" :class="`${disabled ? disabledClass : activeClass} ${classes} flex items-center justify-center`">{{ text }} <i v-if="loading" class="pi pi-spin pi-spinner" style="font-size: 1rem; margin-left: 5px;"></i></button>
     </div>
 </template>
 <script setup>
