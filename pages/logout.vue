9c400ce862e653789ee66aa9e8dc101381c8a895
mosabberul55 <mosabberulislam55@gmail.com>
2024-02-27 16:08:54 +0600

    logout

--- pages/logout.vue
+++ pages/logout.vue
@@ -1,6 +1,7 @@
 <script setup>
 definePageMeta({
-  layout: false
+  layout: false,
+  middleware: ['auth']
 })
 const route = useRoute()
 const router = useRouter()
@@ -9,18 +10,12 @@ const authStore = useAuthStore()
 // const secret = route.query.secret
 
 
-if (authStore.isLoggedIn) {
-  console.log('Logging out...')
-  await authStore.logout()
-  await router.push('/login')
-} else {
-  console.log('Invalid request')
-  router.push('/')
-}
+authStore.logout()
+router.push('/login')
 </script>
 <template>
   <div>
-    <div class="container" >
+    <div class="container">
       <div class="logout-card">
         <h4 class="text-red-600 text-3xl">Logging out...</h4>
       </div>
