6bde3daaaa925ae17f9b5c4779fef5840ebb8b9e
mosabberul55 <mosabberulislam55@gmail.com>
2024-02-27 15:13:00 +0600

    auth middleware fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    // const token = useCookie('token')
-    // return navigateTo('/?token='+token.value)
-    // const authStore = useAuthStore()
-    // if (!authStore.isLoggedIn) {
-    //     return navigateTo('/login?next=' + to.path)
-    // }
-    // if (!token.value) {
-    //     return navigateTo('/login?next=' + to.path)
-    // }
+    const authStore = useAuthStore()
+    if (!authStore.isLoggedIn) {
+        return navigateTo('/login?next=' + to.path)
+    }
 })
