472db03a176a260f66a4f90bf55158e9fbe675bd
Anik Das <dasssanik124102@gmail.com>
2024-05-28 11:53:20 +0600

    Plan dynamic content done

--- components/home/FunnelPricing.vue
+++ components/home/FunnelPricing.vue
@@ -4,36 +4,50 @@
       <section-heading tag="Pricing" title="Simple, Transparent Pricing"
                        sub-title="We believe Untitled should be accessible to all companies, no matter the size." />
     </div>
-    <div class="md:flex justify-center items-center  gap-x-16 pt-12">
-       <template v-for="(item, i) in pricing" :key="i">
-         <div class="bg-white rounded-2xl shadow-xl w-full  lg:w-[36%] my-4 p-8">
-           <div class="text-center pb-8">
-             <p class="text-[24px] xl:text-[30px] font-[600] textColor">{{ item.title }}</p>
-             <p class="text-[16px] xl:text-[20px] font-[600] pb-2 textColor">{{ item.tag }}</p>
-             <p class="text-[12px] xl:text-[16px] font-[400] textSecondary">{{ item.subTitle }}</p>
-           </div>
-           <div v-for="(context ,i) in item.context" :key="i" class="pt-2 flex items-center gap-x-2">
-             <img loading="lazy" class="h-[20px]" src="/pricing/check.svg" alt="image"/>
-             <p class="text-[14px] xl:text-[16px] font-[400] textSecondary">{{ context.label }}</p>
-           </div>
-           <div class="pt-5">
-             <template v-if="!authStore.isLoggedIn">
-               <nuxt-link to="/login">
-                 <div class="buttonHover  hover:ring-1 hover:ring-[white] text-white flex justify-center items-center bg-[#5A78AD] gap-x-1 rounded-md  py-1.5 hover:cursor-pointer">
-                   <p class="font-[600] text-center text-[16px]">Get started</p>
-                 </div>
-               </nuxt-link>
-             </template>
-             <template v-if="authStore.isLoggedIn">
-               <nuxt-link to="/shop">
-                 <div class="buttonHover  hover:ring-1 hover:ring-[white] text-white flex justify-center items-center bg-[#5A78AD] gap-x-1 rounded-md  py-1.5 hover:cursor-pointer">
-                   <p class="font-[600] text-center text-[16px]">Get started</p>
-                 </div>
-               </nuxt-link>
-             </template>
-           </div>
-         </div>
-       </template>
+    <div v-if="items && items.length">
+      <div class="grid grid-cols-1 md:grid-cols-3 pt-12 gap-x-12">
+        <div v-for="(item, i) in items" :key="i" class="">
+          <div class="bg-white rounded-2xl shadow-xl w-full  my-4 p-8">
+            <div class="text-center pb-8">
+              <p class="text-[24px] xl:text-[30px] font-[600] textColor">{{ item.title }}</p>
+              <div v-if="item.discount" class="flex justify-center items-center gap-2">
+                <p class="text-[16px] xl:text-[20px] font-[600] pb-2 textColor">{{ item.price - item.discount }}</p>
+                <p class="text-[16px] xl:text-[20px] font-[600] pb-2 text-gray-400 line-through">{{ item.price }}</p>
+                <p class="text-[16px] xl:text-[20px] font-[600] pb-2 textColor">BDT</p>
+              </div>
+              <div v-else>
+                <p class="text-[16px] xl:text-[20px] font-[600] pb-2 textColor">{{ item.price }} BDT</p>
+              </div>
+              <p class="text-[12px] xl:text-[16px] font-[400] textSecondary">{{ item.duration }} Days</p>
+            </div>
+            <div v-for="(feature ,i) in item.features" :key="i" class="pt-2 flex items-center gap-x-2">
+              <img loading="lazy" class="h-[20px]" src="/pricing/check.svg" alt="image"/>
+              <p class="text-[14px] xl:text-[16px] font-[400] textSecondary">{{ feature }}</p>
+            </div>
+            <div class="pt-5">
+              <template v-if="!authStore.isLoggedIn">
+                <nuxt-link to="/login">
+                  <div class="buttonHover  hover:ring-1 hover:ring-[white] text-white flex justify-center items-center bg-[#5A78AD] gap-x-1 rounded-md  py-1.5 hover:cursor-pointer">
+                    <p class="font-[600] text-center text-[16px]">Get started</p>
+                  </div>
+                </nuxt-link>
+              </template>
+              <template v-if="authStore.isLoggedIn">
+                <nuxt-link to="/shop">
+                  <div class="buttonHover  hover:ring-1 hover:ring-[white] text-white flex justify-center items-center bg-[#5A78AD] gap-x-1 rounded-md  py-1.5 hover:cursor-pointer">
+                    <p class="font-[600] text-center text-[16px]">Get started</p>
+                  </div>
+                </nuxt-link>
+              </template>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else class="md:flex justify-center pt-6 md:pt-8">
+      <div class="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert">
+        <span class="font-medium">No plans available!</span>
+      </div>
     </div>
   </div>
 </template>
@@ -41,6 +55,22 @@
 <script setup>
 import SectionHeading from "~/components/common/SectionHeading.vue";
 const authStore = useAuthStore();
+
+const items = ref([]);
+const isPlansLoading = ref(false);
+
+const getPlans = async () => {
+  isPlansLoading.value = true;
+  const { data, pending, error, refresh } = await getData('/plans', 'subscription');
+  if (error && error.value) {
+    console.log(error.value);
+  } else {
+    items.value = data.value.data;
+  }
+  isPlansLoading.value = false;
+};
+getPlans();
+
 const pricing = ref([
   { title: 'Free', subTitle: 'Billed monthly.', tag:'Basic plan',
     context:[
